30818173e57be5662e9037bce7a25ec956a4af0f
Ashley Brinson <m3ac-brinsona@m3as.com>
2021-11-22 15:52:24 -0500

    Completes module 6

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ItemController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var itemToRemove = _context.Items.Where(x => x.Id == Id).FirstOrDefault();
+            _context.Items.Remove(itemToRemove);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
